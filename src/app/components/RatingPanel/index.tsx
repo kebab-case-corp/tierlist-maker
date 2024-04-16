aa5caa4e4c924765555d344505d4ee158605bebb
AmaruZ <a.zirouel@gmail.com>
2024-04-16 10:03:10 +0200

    style: update rating panel styles

--- src/app/components/RatingPanel/index.tsx
+++ src/app/components/RatingPanel/index.tsx
@@ -1,21 +1,33 @@
-"use client"
-import { useState } from "react"
-import RatingSlider from "../RatingSlider"
-import {data} from "@/app/lib/data"
-import { tierList } from "@/app/lib/data"
-import styles from "@/app/components/RatingPanel/index.module.css"
-
+'use client'
+import { useState } from 'react'
+import RatingSlider from '../RatingSlider'
+import { data } from '@/app/lib/data'
+import { tierList } from '@/app/lib/data'
+import styles from '@/app/components/RatingPanel/index.module.css'
 
 function RatingPanel({}) {
     const [rating, setRating] = useState(data.ratings)
-    console.log(rating)
-  return (
-    <div className={styles.wrapper}><img src={data.image} alt="" className={styles.img}></img>
-    {tierList.criterias.map((criteria, i) => <RatingSlider name={criteria.name} setRating={setRating} index={i} rating={rating} key={i} ></RatingSlider>)}
-    <button>Ajouter</button>
-    <p className="rating">{rating.reduce((sum, currentRate) => sum + currentRate, 0)}</p>
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <img src={data.image} alt="" className={styles.image}></img>
+            {tierList.criterias.map((criteria, i) => (
+                <RatingSlider
+                    name={criteria.name}
+                    setRating={setRating}
+                    index={i}
+                    rating={rating}
+                    key={i}
+                ></RatingSlider>
+            ))}
+
+            <div className={styles.wrapper}>
+                <p className={styles['total-rating']}>
+                    {rating.reduce((sum, currentRate) => sum + currentRate, 0)}
+                </p>
+                <button className={styles.button}>Ajouter</button>
+            </div>
+        </div>
+    )
 }
 
 export default RatingPanel
--- src/app/components/RatingSlider/index.tsx
+++ src/app/components/RatingSlider/index.tsx
@@ -1,5 +1,5 @@
-import {ChangeEvent, useState} from "react"
-import styles from "@/app/components/RatingSlider/index.module.css"
+import { ChangeEvent, useState } from 'react'
+import styles from '@/app/components/RatingSlider/index.module.css'
 
 type RatingSliderProps = {
     name: string
@@ -8,22 +8,36 @@ type RatingSliderProps = {
     rating: number[]
 }
 
-
-function RatingSlider({name, setRating, index, rating}:RatingSliderProps) {
+function RatingSlider({ name, setRating, index, rating }: RatingSliderProps) {
     const [value, setValue] = useState(0)
-    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const newRating = [...rating]
         newRating[index] = parseFloat(e.target.value)
         setValue(parseFloat(e.target.value))
         setRating(newRating)
-    } 
-  return (
-    <div className={styles.div}>
-        <label htmlFor={name}>{name}</label>
-        <input type="range" min="0" max="5" value={value} className="slider" id={name} step={0.25} onChange={handleChange}/>
-        <output htmlFor={name}>{value}</output>
-    </div>
-  )
+    }
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <label htmlFor={name} className={styles.label}>
+                    {name}
+                </label>
+                <output htmlFor={name} className={styles.output}>
+                    {value}
+                </output>
+            </div>
+            <input
+                type="range"
+                min="0"
+                max="5"
+                value={value}
+                className={styles.input}
+                id={name}
+                step={0.25}
+                onChange={handleChange}
+            />
+        </div>
+    )
 }
 
 export default RatingSlider
