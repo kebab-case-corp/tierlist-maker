91f91d544e0339dd582c9813d7f1d4d60c583268
Akaiin <akain1612@gmail.com>
2024-04-17 13:01:58 +0200

    feat: create UnratedBox upload component

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: dot.env.FIRESTORE_API_KEY,
+  apiKey: process.env.FIRESTORE_API_KEY,
   authDomain: "tier-list-maker-d6ed5.firebaseapp.com",
   projectId: "tier-list-maker-d6ed5",
   storageBucket: "tier-list-maker-d6ed5.appspot.com",
@@ -15,3 +16,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage(app);
--- src/app/components/UnratedBox/index.tsx
+++ src/app/components/UnratedBox/index.tsx
@@ -1,10 +1,62 @@
-import React from 'react'
-import { tierlists } from '@/app/lib/data'
+"use client";
+
+import { useEffect, useState } from "react";
+import { tierlists } from "@/app/lib/data";
+import { storage } from "../../../../firebase-config";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import Resizer from "react-image-file-resizer";
 
 function UnratedBox() {
+  const [url, setUrl] = useState<string>("");
+  const resizeFile = (file: File) =>
+    new Promise((resolve) => {
+      Resizer.imageFileResizer(
+        file,
+        300,
+        300,
+        "JPEG",
+        100,
+        0,
+        (uri) => {
+          resolve(uri);
+        },
+        "file"
+      );
+    });
+
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files ? event.target.files[0] : null;
+    if (!file) return;
+    const fileName = file.name.split(".");
+    const extension = fileName[fileName.length - 1];
+    if (extension.includes("jpg" || "png" || "jpeg" || "webp")) {
+      console.log(file);
+      const newImage = await resizeFile(file);
+      const fileRef = ref(storage, `tierlist/${Date.now() + file.name}`);
+      await uploadBytes(fileRef, newImage as File);
+      const uploadedUrl = await getDownloadURL(fileRef);
+      setUrl(uploadedUrl);
+    }
+     else {
+      console.log(`ceci n'est pas une image`)
+     }
+  };
+
   return (
-    <div>{tierlists[0].unratedItems.map(unratedItem => <img src={unratedItem.image}></img>)}</div>
-  )
+    <div>
+      <div>
+        {tierlists[0].unratedItems.map((unratedItem) => (
+          <img src={unratedItem.image} key={unratedItem.id}></img>
+        ))}
+      </div>
+      <div>
+        <input type="file" onChange={handleFileChange} />
+        {url && <img src={url} alt="Uploaded" />}
+      </div>
+    </div>
+  );
 }
 
-export default UnratedBox
+export default UnratedBox;
