527a88fb03e4077f98c31cdf4f83cb3edcd6f0d4
Akaiin <akain1612@gmail.com>
2024-04-23 16:29:48 +0200

    feat: update tierlistid page

--- src/app/tierlist/[tierlistid]/page.tsx
+++ src/app/tierlist/[tierlistid]/page.tsx
@@ -1,11 +1,28 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
-import React from "react";
+import { Tierlist } from "@/app/lib/definitions";
+import { collection, getDocs } from "firebase/firestore";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { db } from "../../../../firebase-config";
 
 function Page() {
-  const path = usePathname();
-  return <div>{path}</div>;
+  const [tierlist, setTierlist] = useState<Tierlist>();
+  const { tierlistid } = useParams<{ tierlistid: string }>();
+
+  useEffect(() => {
+    const fetchTierlist = async () => {
+      const querySnapshot = await getDocs(collection(db, "tierlists"));
+      querySnapshot.forEach((doc) => {
+        if (doc.id === tierlistid) {
+          setTierlist(doc.data() as Tierlist);
+        }
+      });
+    };
+    fetchTierlist();
+  }, []);
+  console.log(tierlist);
+  return <div>{tierlistid}</div>;
 }
 
 export default Page;
