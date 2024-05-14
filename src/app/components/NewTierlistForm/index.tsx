b3f04f597993f8cf9063d69a998bba02c608bbad
Akaiin <akain1612@gmail.com>
2024-05-14 16:07:19 +0200

    feat: create modal form
    create delete object btn

--- src/app/components/NewTierlistForm/index.tsx
+++ src/app/components/NewTierlistForm/index.tsx
@@ -1,88 +1,111 @@
-import { Criteria, Tier, Tierlist } from '@/app/lib/definitions';
-import React, { ChangeEvent, FormEvent, useState } from 'react';
-import styles from '@/app/components/NewTierlistForm/index.module.css';
-import TierlistCriteriasEditor from './criterias-editor';
-import TierlistTiersEditor from './tiers-editor';
-import { addNewTierlist } from '@/app/lib/data';
+import { Criteria, Tier, Tierlist } from "@/app/lib/definitions";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import styles from "@/app/components/NewTierlistForm/index.module.css";
+import TierlistCriteriasEditor from "./criterias-editor";
+import TierlistTiersEditor from "./tiers-editor";
+import { addNewTierlist } from "@/app/lib/data";
 
 function NewTierlistForm({ userId }: { userId: string }) {
-	const [tiers, setTiers] = useState<Tier[]>([
-		{ name: 'S', max: 20 },
-		{ name: 'A', max: 16 },
-		{ name: 'B', max: 12 },
-		{ name: 'C', max: 8 },
-		{ name: 'D', max: 4 },
-	]);
+    const [tiers, setTiers] = useState<Tier[]>([
+        { name: "S", max: 20 },
+        { name: "A", max: 16 },
+        { name: "B", max: 12 },
+        { name: "C", max: 8 },
+        { name: "D", max: 4 },
+    ]);
 
-	const [criterias, setCriterias] = useState<Criteria[]>([
-		{ name: '', maxRate: 5, description: '' },
-		{ name: '', maxRate: 5, description: '' },
-		{ name: '', maxRate: 5, description: '' },
-		{ name: '', maxRate: 5, description: '' },
-	]);
+    const [criterias, setCriterias] = useState<Criteria[]>([
+        { name: "", maxRate: 5, description: "" },
+        { name: "", maxRate: 5, description: "" },
+        { name: "", maxRate: 5, description: "" },
+        { name: "", maxRate: 5, description: "" },
+    ]);
 
-	const [tierlist, setTierlist] = useState<Tierlist>({
-		userId: userId,
-		name: '',
-		createdAt: new Date().getTime(),
-		description: '',
-		criterias: criterias,
-		tiers: tiers,
-	});
+    const [tierlist, setTierlist] = useState<Tierlist>({
+        userId: userId,
+        name: "",
+        createdAt: new Date().getTime(),
+        description: "",
+        criterias: criterias,
+        tiers: tiers,
+    });
 
-	const handleChange = (
-		e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-		field: 'name' | 'description'
-	) => {
-		let prevTierlist = { ...tierlist };
-		prevTierlist[field] = e.target.value;
-		setTierlist(prevTierlist);
-	};
+    const handleChange = (
+        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+        field: "name" | "description"
+    ) => {
+        let prevTierlist = { ...tierlist };
+        prevTierlist[field] = e.target.value;
+        setTierlist(prevTierlist);
+    };
 
-	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		const prevTierlist = { ...tierlist };
-		prevTierlist.criterias = criterias;
-		prevTierlist.tiers = tiers;
-		prevTierlist.createdAt = new Date().getTime();
-		try {
-			const docRef = await addNewTierlist(prevTierlist);
-			prevTierlist.id = docRef.id;
-			setTierlist(prevTierlist);
-		} catch (error) {
-			console.error(error);
-		}
-	};
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const prevTierlist = { ...tierlist };
+        prevTierlist.criterias = criterias;
+        prevTierlist.tiers = tiers;
+        prevTierlist.createdAt = new Date().getTime();
+        try {
+            const docRef = await addNewTierlist(prevTierlist);
+            prevTierlist.id = docRef.id;
+            setTierlist(prevTierlist);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-	return (
-		<div className={styles.container}>
-			<h1 className={styles.title}>Nouvelle Tierlist</h1>
-			<form className={styles.form} onSubmit={handleSubmit}>
-				<label htmlFor='tierlist-name'>Nom:</label>
-				<input
-					type='text'
-					id='tierlist-name'
-					className={styles.input}
-					onChange={(e) => handleChange(e, 'name')}
-					required></input>
-				<label htmlFor='tierlist-description'>Description</label>
-				<textarea
-					name='description'
-					id='tierlist-description'
-					className={styles.input}
-					cols={30}
-					rows={5}
-					onChange={(e) => handleChange(e, 'description')}></textarea>
-				<TierlistTiersEditor setTiers={setTiers} tiers={tiers}></TierlistTiersEditor>
-				<TierlistCriteriasEditor
-					setCriterias={setCriterias}
-					criterias={criterias}></TierlistCriteriasEditor>
-				<button type='submit' className={styles.submit}>
-					Créer
-				</button>
-			</form>
-		</div>
-	);
+    const [modal, setModal] = useState(false);
+
+    const handleModalClick = () => {
+        setModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setModal(false);
+    };
+    return (
+        <div>
+            <button onClick={handleModalClick}>+</button>
+            {modal && (
+                <div className={styles.background}>
+                    <div className={styles.container}>
+                        <button onClick={handleCloseModal}>X</button>
+                        <h1 className={styles.title}>Nouvelle Tierlist</h1>
+                        <form className={styles.form} onSubmit={handleSubmit}>
+                            <label htmlFor="tierlist-name">Nom:</label>
+                            <input
+                                type="text"
+                                id="tierlist-name"
+                                className={styles.input}
+                                onChange={(e) => handleChange(e, "name")}
+                                required
+                            ></input>
+                            <label htmlFor="tierlist-description">Description</label>
+                            <textarea
+                                name="description"
+                                id="tierlist-description"
+                                className={styles.input}
+                                cols={30}
+                                rows={5}
+                                onChange={(e) => handleChange(e, "description")}
+                            ></textarea>
+                            <TierlistTiersEditor
+                                setTiers={setTiers}
+                                tiers={tiers}
+                            ></TierlistTiersEditor>
+                            <TierlistCriteriasEditor
+                                setCriterias={setCriterias}
+                                criterias={criterias}
+                            ></TierlistCriteriasEditor>
+                            <button type="submit" className={styles.submit}>
+                                Créer
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default NewTierlistForm;
--- src/app/components/TierList/index.tsx
+++ src/app/components/TierList/index.tsx
@@ -6,10 +6,13 @@ import { useTierlistStore } from "@/app/store/tierlist-store";
 import { useItemsStore } from "@/app/store/items-store";
 import Image from "next/image";
 import { calculateItemScore } from "@/app/lib/utils";
+import { db, storage } from "../../../../firebase-config";
+import { deleteObject, ref } from "firebase/storage";
+import { collection, deleteDoc, doc } from "firebase/firestore";
 
 function TierList() {
     const { tierlist } = useTierlistStore((state) => state);
-    const { items, selectedItem, setSelectedItem } = useItemsStore((state) => state);
+    const { items, selectedItem, setSelectedItem, removeItem } = useItemsStore((state) => state);
 
     const groupedItems = tierlist
         ? groupItemsByTier(
@@ -17,6 +20,17 @@ function TierList() {
               tierlist
           )
         : {};
+
+    const handleDeleteImg = (item: Item) => {
+        removeItem(item.id as string);
+        const imageRef = ref(storage, item.imageUrl);
+        deleteObject(imageRef)
+            .then(() => {
+                deleteDoc(doc(collection(db, "tierlists", tierlist?.id, "items"), item.id));
+            })
+            .catch((error) => console.error(error));
+    };
+
     return (
         <div className={styles.tierlist}>
             {Object.entries(groupedItems).map(([tierName, tierItems]) => (
@@ -26,6 +40,7 @@ function TierList() {
                         {tierItems.map((item) => (
                             <li key={item.id} onClick={() => setSelectedItem(item)}>
                                 <p>{tierlist && calculateItemScore(item, tierlist?.criterias)}</p>
+                                <button onClick={() => handleDeleteImg(item)}>x</button>
                                 <Image
                                     className={`${styles.img} ${
                                         selectedItem?.id === item.id && styles.selected
@@ -35,6 +50,7 @@ function TierList() {
                                     width={100}
                                     height={100}
                                     priority={false}
+                                    unoptimized
                                 />
                             </li>
                         ))}
